88d3496bd5bbeeb507557834c002c377305f0950
Kuratar <ednkage@gmail.com>
2021-04-25 20:51:29 -0700

    Deleted unused import statement

--- src/BrowseByGenreServlet.java
+++ src/BrowseByGenreServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-import javax.xml.transform.Result;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
