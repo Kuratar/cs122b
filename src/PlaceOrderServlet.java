f71817892c315a3815101afe1c2311177bf57819
Kuratar <ednkage@gmail.com>
2021-05-06 01:04:10 -0700

    Revised to have preparedstatement usage and deleted print statements for debugging

--- src/PlaceOrderServlet.java
+++ src/PlaceOrderServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Date;
@@ -41,45 +42,44 @@ public class PlaceOrderServlet extends HttpServlet {
         String card_number = request.getParameter("card-number");
         String expiry_date = request.getParameter("expiry-date");
 
-        System.out.println(first_name);
-        System.out.println(last_name);
-        System.out.println(card_number);
-        System.out.println(expiry_date);
+        // query to check if user exists within database
+        String userQuery = "select *\n" +
+                "from creditcards\n" +
+                "where id=? and expiration=?";
+        String query2 = "select id\n" +
+                "from customers\n" +
+                "where firstName=? and lastName=?";
+        String query3 = "select id\n" +
+                "from movies\n" +
+                "where title=?";
+        String updateQuery = "INSERT INTO sales (customerId, movieId, saleDate) VALUES (?, ?, ?)";
+
         JsonObject responseJsonObject = new JsonObject();
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(userQuery);
+             PreparedStatement statement2 = conn.prepareStatement(query2);
+             PreparedStatement statement3 = conn.prepareStatement(query3);
+             PreparedStatement statement4 = conn.prepareStatement(updateQuery)) {
+            // place in id and expiration
+            statement.setString(1, card_number);
+            statement.setString(2, expiry_date);
 
-        try (Connection conn = dataSource.getConnection()) {
-            // Declare our statement
-            Statement statement = conn.createStatement();
-
-            // query to check if user exists within database
-            String userQuery = "select *\n" +
-                    "from creditcards\n" +
-                    "where id='" + card_number + "' and " +
-                    "expiration='" + expiry_date + "'";
-
-            System.out.println(userQuery);
             // Perform check user query
-            ResultSet rs = statement.executeQuery(userQuery);
-
+            ResultSet rs = statement.executeQuery();
             if (rs.next()) {
-
                 responseJsonObject.addProperty("status", "success");
                 responseJsonObject.addProperty("message", "success");
 
-                String query2 = "select id\n" +
-                        "from customers\n" +
-                        "where firstName='" + first_name + "' and " +
-                        "lastName='" + last_name + "'";
-                System.out.println(query2);
+                // place in firstName and lastName
+                statement2.setString(1, first_name);
+                statement2.setString(2, last_name);
 
-                ResultSet rs2 = statement.executeQuery(query2);
-
+                ResultSet rs2 = statement2.executeQuery();
                 int customerId = 0;
                 if (rs2.next())
                 {
                     customerId = rs2.getInt("id");
                 }
-
 
                 HttpSession session = request.getSession();
                 Date date = new Date();
@@ -97,42 +97,34 @@ public class PlaceOrderServlet extends HttpServlet {
                     total += quantity * 10;
                     String movieId = "";
 
-                    String query3 = "select id\n" +
-                            "from movies\n" +
-                            "where title='" + title + "'";
+                    // place in movie title
+                    statement3.setString(1, title);
 
-                    ResultSet rs3 = statement.executeQuery(query3);
-                    System.out.println(query3);
+                    ResultSet rs3 = statement3.executeQuery();
                     if (rs3.next())
                     {
                         movieId = rs3.getString("id");
-                        System.out.println(movieId);
                     }
 
                     for (int i = 0; i<quantity; i++)
                     {
-                        String updateQuery = "INSERT INTO sales (customerId, movieId, saleDate) VALUES (" + customerId + ", '" + movieId + "', '" + date_str + "')";
-                        System.out.println(updateQuery);
-                        statement.executeUpdate(updateQuery);
-
+                        statement4.setInt(1, customerId);
+                        statement4.setString(2, movieId);
+                        statement4.setString(3, date_str);
+                        statement4.executeUpdate();
                     }
 
                     rs3.close();
                 }
                 responseJsonObject.addProperty("total", total);
                 responseJsonObject.addProperty("sale_info", sale_info);
-                System.out.println(total);
-                System.out.println(sale_info);
 
                 rs2.close();
                 session.removeAttribute("previousItems");
-
             } else {
-
                 responseJsonObject.addProperty("status", "fail");
                 responseJsonObject.addProperty("message", "Incorrect payment information, please try again.");
             }
-
             rs.close();
             statement.close();
             response.setStatus(200);
@@ -145,7 +137,6 @@ public class PlaceOrderServlet extends HttpServlet {
             // set response status to 500 (Internal Server Error)
             response.setStatus(500);
         }
-
         response.getWriter().write(responseJsonObject.toString());
     }
 }
