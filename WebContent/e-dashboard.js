23c3c0990baaff46c0224477f710ff1f3815bd87
Kuratar <ednkage@gmail.com>
2021-05-08 01:14:16 -0700

    Revised _dashboard.js to send users to e-dashboard.html upon successful login. Created e-dashboard html page. Created e-dashboard.js, currently only handles adding stars. Created InsertStarServlet.java servlet for adding stars into the database.

---
+++ WebContent/e-dashboard.js
@@ -0,0 +1,57 @@
+/**
+ * Retrieve parameter from request URL, matching by parameter name
+ * @param target String
+ * @returns {*}
+ */
+function getParameterByName(target) {
+    // Get request URL
+    let url = window.location.href;
+    // Encode target parameter name to url encoding
+    target = target.replace(/[\[\]]/g, "\\$&");
+
+    // Ues regular expression to find matched parameter value
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    // Return the decoded parameter value
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+function insertStar() {
+    let star_form = $("#star_form");
+    let starName = getParameterByName("name");
+    let birthYear = getParameterByName("birth_year");
+    jQuery.ajax({
+        dataType: "json", // Setting return data type
+        method: "GET", // Setting request method
+        //url: "api/insert-star?name=" + starName + "&birthYear=" + birthYear, // Setting request url, which is mapped by MoviesServlet in Movies.java
+        url: "api/insert-star?" + star_form.serialize(),
+        success: (resultData) => handleInsertStarResult(resultData) // Setting callback function to handle data returned successfully by the MoviesServlet
+    });
+
+    return false;
+}
+
+
+function handleInsertStarResult(resultData) {
+    console.log("handle insert star response");
+    console.log(resultData);
+    console.log(resultData["status"]);
+
+    if (resultData["status"] === "success") {
+        $("#star_form_message").text(resultData["message"]);
+    }
+    else {
+        $("#star_form_message").text("Error adding new star");
+        console.log(resultData["errorMessage"]);
+    }
+}
+
+
+function insertMovie() {
+
+    return false;
+}
---
+++ src/InsertStarServlet.java
@@ -0,0 +1,75 @@
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import javax.xml.transform.Result;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet(name = "InsertStarServlet", urlPatterns = "/api/insert-star")
+public class InsertStarServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    // Create a dataSource which registered in web.
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedbexample");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name");
+        String birthYear = request.getParameter("birth_year");
+
+        String query = "select max(id) from stars";
+
+        String query2 = "INSERT INTO stars (id, name, birthYear) VALUES (?, ?, ?)";
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(query);
+             PreparedStatement statement2 = conn.prepareStatement(query2)) {
+            JsonObject jsonObject = new JsonObject();
+
+            // get max id and rs.next to the row
+            ResultSet rs = statement.executeQuery();    rs.next();
+            // convert the digit part of id to int
+            int maxId = Integer.parseInt(rs.getString("max(id)").substring(2));
+            // set parameters for query2 and execute
+            statement2.setString(1, "nm"+maxId+1);
+            statement2.setString(2, name);
+            if (birthYear.equals("")) { statement2.setString(3, null); }
+            else { statement2.setInt(3, Integer.parseInt(birthYear)); }
+            statement2.executeUpdate();
+
+            jsonObject.addProperty("status", "success");
+            jsonObject.addProperty("message", "Successfully added new star");
+
+            rs.close();
+            statement.close();
+            statement2.close();
+            response.setStatus(200);
+            response.getWriter().write(jsonObject.toString());
+        } catch (Exception e) {
+            // write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            response.getWriter().write(jsonObject.toString());
+
+            // set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        }
+    }
+}
