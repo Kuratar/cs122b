93710f19c7a42945f59855d5d9a9e32485168688
Kuratar <ednkage@gmail.com>
2021-06-04 23:34:41 -0700

    disabled loginfilter for p5

--- src/LoginFilter.java
+++ src/LoginFilter.java
@@ -1,92 +1,92 @@
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-
-/**
- * Servlet Filter implementation class LoginFilter
- */
-@WebFilter(filterName = "LoginFilter", urlPatterns = "/*")
-public class LoginFilter implements Filter {
-    private final ArrayList<String> allowedURIs = new ArrayList<>();
-    private final ArrayList<String> staffURIs = new ArrayList<>();
-    private final ArrayList<String> p5URIs = new ArrayList<>();
-
-    /**
-     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-     */
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-        System.out.println("LoginFilter: " + httpRequest.getRequestURI());
-
-        // Check if this URL is allowed to access without logging in
-        if (this.isUrlAllowedWithoutLogin(httpRequest.getRequestURI())) {
-            // if the current user is regular user and the accessing url is a staff url, send to error 401 html
-            if (httpRequest.getSession().getAttribute("user") != null &&
-                httpRequest.getSession().getAttribute("employee") == null &&
-                this.isStaffUrl(httpRequest.getRequestURI())) {
-                httpResponse.sendRedirect("error-404.html");
-            }
-            else {
-                // Keep default action: pass along the filter chain
-                chain.doFilter(request, response);
-            }
-            return;
-        }
-        else if (this.isP5Url(httpRequest.getRequestURI())) {
-            chain.doFilter(request, response);
-        }
-
-        // Redirect to login page if the "user" attribute doesn't exist in session
-        if (httpRequest.getSession().getAttribute("user") == null &&
-            httpRequest.getSession().getAttribute("employee") == null) {
-            httpResponse.sendRedirect("login.html");
-        } else {
-            chain.doFilter(request, response);
-        }
-    }
-
-    private boolean isUrlAllowedWithoutLogin(String requestURI) {
-        /*
-         Setup your own rules here to allow accessing some resources without logging in
-         Always allow your own login related requests(html, js, servlet, etc..)
-         You might also want to allow some CSS files, etc..
-         */
-        return allowedURIs.stream().anyMatch(requestURI.toLowerCase()::endsWith);
-    }
-
-    private boolean isStaffUrl(String requestURI) {
-        return staffURIs.stream().anyMatch(requestURI.toLowerCase()::endsWith);
-    }
-
-    private boolean isP5Url(String requestURI) {
-        return p5URIs.stream().anyMatch(requestURI.toLowerCase()::endsWith);
-    }
-
-    public void init(FilterConfig fConfig) {
-        allowedURIs.add("login.html");
-        allowedURIs.add("login.js");
-        allowedURIs.add("api/login");
-        allowedURIs.add("_dashboard.html");
-        allowedURIs.add("_dashboard.js");
-        allowedURIs.add("api/loginstaff");
-        staffURIs.add("_dashboard.html");
-        staffURIs.add("_dashboard.js");
-        staffURIs.add("api/loginstaff");
-
-        // project 5
-        p5URIs.add("auto-search.html");
-        p5URIs.add("auto-search.js");
-        p5URIs.add("api/auto-search");
-    }
-
-    public void destroy() {
-        // ignored.
-    }
-
-}
+//import javax.servlet.*;
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//
+///**
+// * Servlet Filter implementation class LoginFilter
+// */
+//@WebFilter(filterName = "LoginFilter", urlPatterns = "/*")
+//public class LoginFilter implements Filter {
+//    private final ArrayList<String> allowedURIs = new ArrayList<>();
+//    private final ArrayList<String> staffURIs = new ArrayList<>();
+//    private final ArrayList<String> p5URIs = new ArrayList<>();
+//
+//    /**
+//     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+//     */
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//            throws IOException, ServletException {
+//        HttpServletRequest httpRequest = (HttpServletRequest) request;
+//        HttpServletResponse httpResponse = (HttpServletResponse) response;
+//
+//        System.out.println("LoginFilter: " + httpRequest.getRequestURI());
+//
+//        // Check if this URL is allowed to access without logging in
+//        if (this.isUrlAllowedWithoutLogin(httpRequest.getRequestURI())) {
+//            // if the current user is regular user and the accessing url is a staff url, send to error 401 html
+//            if (httpRequest.getSession().getAttribute("user") != null &&
+//                httpRequest.getSession().getAttribute("employee") == null &&
+//                this.isStaffUrl(httpRequest.getRequestURI())) {
+//                httpResponse.sendRedirect("error-404.html");
+//            }
+//            else {
+//                // Keep default action: pass along the filter chain
+//                chain.doFilter(request, response);
+//            }
+//            return;
+//        }
+//        else if (this.isP5Url(httpRequest.getRequestURI())) {
+//            chain.doFilter(request, response);
+//        }
+//
+//        // Redirect to login page if the "user" attribute doesn't exist in session
+//        if (httpRequest.getSession().getAttribute("user") == null &&
+//            httpRequest.getSession().getAttribute("employee") == null) {
+//            httpResponse.sendRedirect("login.html");
+//        } else {
+//            chain.doFilter(request, response);
+//        }
+//    }
+//
+//    private boolean isUrlAllowedWithoutLogin(String requestURI) {
+//        /*
+//         Setup your own rules here to allow accessing some resources without logging in
+//         Always allow your own login related requests(html, js, servlet, etc..)
+//         You might also want to allow some CSS files, etc..
+//         */
+//        return allowedURIs.stream().anyMatch(requestURI.toLowerCase()::endsWith);
+//    }
+//
+//    private boolean isStaffUrl(String requestURI) {
+//        return staffURIs.stream().anyMatch(requestURI.toLowerCase()::endsWith);
+//    }
+//
+//    private boolean isP5Url(String requestURI) {
+//        return p5URIs.stream().anyMatch(requestURI.toLowerCase()::endsWith);
+//    }
+//
+//    public void init(FilterConfig fConfig) {
+//        allowedURIs.add("login.html");
+//        allowedURIs.add("login.js");
+//        allowedURIs.add("api/login");
+//        allowedURIs.add("_dashboard.html");
+//        allowedURIs.add("_dashboard.js");
+//        allowedURIs.add("api/loginstaff");
+//        staffURIs.add("_dashboard.html");
+//        staffURIs.add("_dashboard.js");
+//        staffURIs.add("api/loginstaff");
+//
+//        // project 5
+//        p5URIs.add("auto-search.html");
+//        p5URIs.add("auto-search.js");
+//        p5URIs.add("api/auto-search");
+//    }
+//
+//    public void destroy() {
+//        // ignored.
+//    }
+//
+//}
