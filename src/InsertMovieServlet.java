2b274ab1c134e8c49d0a8881af8c9927b137a4ea
Kuratar <ednkage@gmail.com>
2021-06-05 20:18:09 -0700

    same as previous commit but for single instance

--- src/InsertMovieServlet.java
+++ src/InsertMovieServlet.java
@@ -22,8 +22,8 @@ public class InsertMovieServlet extends HttpServlet {
 
     public void init(ServletConfig config) {
         try {
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedbexample-master");
-            //dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedbexample");
+            //dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedbexample-master");
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedbexample");
         } catch (NamingException e) {
             e.printStackTrace();
         }
